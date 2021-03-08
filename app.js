81fc7f5d8df278213e7a364fa710ee76020f976b
Ricardo Barranqueiro <ricardo.b@rbit.com.br>
2021-03-08 17:14:17 -0300

    Add variable port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.port || 3000;
+
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
-app.listen(3000, () => {
-  console.log("Hello!");  
+app.listen(port, () => {
+  console.log("Listening port ", port);  
 });
